952f9a897b11514d8cef721ae9a8948922d5c3bb
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-03-06 23:20:21 +0530

    added configruations for dataSources and merging resolvers

--- GraphQL_Layer/dist/index.js
+++ GraphQL_Layer/dist/index.js
@@ -1,12 +1,27 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema/employee.js';
-import { resolvers } from './resolver/employee.js';
+import { mergeResolvers } from '@graphql-tools/merge';
+import { employeResolver } from './resolver/employee.js';
+import { leaveResolver } from './resolver/leaves.js';
+import EmployeeAPI from './api/employee.js';
+const resolvers = mergeResolvers([employeResolver, leaveResolver]);
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-});
-console.log(`🚀  Server ready at: ${url}`);
+async function startApolloServer() {
+    const server = new ApolloServer({ typeDefs, resolvers });
+    const { url } = await startStandaloneServer(server, {
+        context: async ({ req }) => {
+            return {
+                dataSources: {
+                    EmployeeAPI: new EmployeeAPI(),
+                },
+            };
+        },
+        listen: { port: 4000 },
+    });
+    console.log(`GraphQL server running at: ${url}`);
+}
+startApolloServer();
