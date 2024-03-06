7c21c6922950c65c4e8c6240bc640884d49e80bc
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-03-06 23:21:42 +0530

    added the resolver methods that call the EmployeeAPI class methods

--- GraphQL_Layer/dist/resolver/employee.js
+++ GraphQL_Layer/dist/resolver/employee.js
@@ -1,17 +1,160 @@
-export const resolvers = {
+export const employeResolver = {
+    SigninResponse: {
+        __resolveType(obj, contextValue, info) {
+            if (obj.token) {
+                return 'SigninSuccess';
+            }
+            if (obj.error) {
+                return 'SigninError';
+            }
+            return null; // GraphQLError is thrown
+        },
+    },
+    applyLeaveResponse: {
+        __resolveType(obj, contextValue, info) {
+            if (obj.message) {
+                return 'applyLeaveSuccess';
+            }
+            if (obj.error) {
+                return 'applyLeaveError';
+            }
+            return null; // GraphQLError is thrown
+        },
+    },
     Query: {
-        async signin(_, args) {
-            const response = await fetch(`http://localhost:3000/api/v1/auth/signin`, {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(args.input),
-            });
-            const data = await response.json();
-            console.log(data);
-            return data;
+        async listAllEmployees(_, args, { dataSources }) {
+            const response = await dataSources.EmployeeAPI.listAllEmployees(args.jwtToken);
+            console.log(response);
+            return 'hi';
+        },
+        async getSignedUser(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.getLoggedInEmployeesDetails(args.jwtToken);
+                return 'response';
+            }
+            catch (error) {
+                console.error(error.message, 'resolver');
+                throw new Error('Internal server error');
+            }
+        },
+        async getEmployeeDetails(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.getEmployeeDetails(args.employeeId, args.jwtToken);
+                console.log(response);
+                return response.data;
+            }
+            catch (error) {
+                console.error(error.message, 'resolver');
+                return error.extensions.response.body;
+            }
+        },
+        // Leave Queries
+        async getAllLeavesOfLoggedInEmployee(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.getAllLeavesOfLoggedInEmployee(args.jwtToken);
+                console.log(response);
+                return response;
+            }
+            catch (error) {
+                console.error(error.message);
+                return error.extensions.response.body;
+            }
+        },
+        async getSpecificMeLeave(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.getSpecificMeLeave(args.leaveId, args.jwtToken);
+                console.log(response);
+                return response;
+            }
+            catch (error) {
+                console.error(error.message);
+                return error.extensions.response.body;
+            }
         }
+    },
+    Mutation: {
+        async registerEmployee(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.registerEmployee(args.input, args.jwtToken);
+                return 'response';
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error('Internal server error');
+            }
+        },
+        async signin(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.signin(args.input);
+                console.log(response, 'here is ti');
+                return response;
+            }
+            catch (error) {
+                console.error(error);
+                return error.extensions.response.body;
+            }
+        },
+        async deleteEmployee(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.deleteEmployee(args.employeeId, args.jwtToken);
+                return 'response';
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error('Internal server error');
+            }
+        },
+        async activateEmployee(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.activateEmployee(args.employeeId, args.jwtToken);
+                return 'response';
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error('Internal server error');
+            }
+        },
+        async updateEmployeeProfile(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.updateEmployeeProfile(args.employeeId, args.jwtToken, args.input);
+                console.log(response);
+                return 'response';
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error('Internal server error');
+            }
+        },
+        async updateMeProfile(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.updateMeProfile(args.jwtToken, args.input);
+                console.log(response);
+                return 'response';
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error('Internal server error');
+            }
+        },
+        async deleteMe(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.deleteMe(args.jwtToken);
+                return 'response';
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error('Internal server error');
+            }
+        },
+        async applyLeave(_, args, { dataSources }) {
+            try {
+                const response = await dataSources.EmployeeAPI.applyLeave(args.input, args.jwtToken);
+                console.log(response);
+                return response;
+            }
+            catch (error) {
+                return error.extensions.response.body;
+            }
+        },
     }
 };
