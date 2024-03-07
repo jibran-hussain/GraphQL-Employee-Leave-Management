c828cef96bb403d809e2b5ab605c87e6f3669e7c
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-03-07 23:20:06 +0530

    created leave schema

--- GraphQL_Layer/dist/schema/leave.js
+++
@@ -1,9 +0,0 @@
-export const typeDefs = `#graphql
-    type leave{
-        id: ID!
-        reason: String
-        dates: [String!]
-        status: status!
-        rejectionReaosn: String
-    }
-`;
