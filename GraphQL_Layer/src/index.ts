55f6c61e9bbdd5350d3cea9725fb5bd44bb650be
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-03-08 22:49:00 +0530

    changed import directroy path

--- GraphQL_Layer/src/index.ts
+++ GraphQL_Layer/src/index.ts
@@ -1,15 +1,25 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema/employee.js';
-import { resolvers } from './resolver/employee.js';
+import { typeDefs } from './schema/schema.js';
+import {mergeResolvers} from '@graphql-tools/merge'
+import { employeResolver } from './resolver/employee.js';
+import { leaveResolver } from './resolver/leaves.js';
+import EmployeeAPI from './api/employee.js';
 
-const server= new ApolloServer({
-    typeDefs,
-    resolvers
-})
+const resolvers=mergeResolvers([employeResolver,leaveResolver])
+
+const server = new ApolloServer({ typeDefs, resolvers })
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+  listen: { port: 4000 },
+  context: async ({req, res}) => {
+    const { cache } = server;
+    const token = req.headers.authorization || '';
+      return {
+        dataSources: {
+          EmployeeAPI: new EmployeeAPI({cache, token}),
+        }
+      };
+    },
+})
+console.log(`Server ready at: ${url}`)
