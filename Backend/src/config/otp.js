5669224d763f7aec5cf6211f238306c2fb2e429a
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-04-03 13:54:00 +0530

    added config file for configuring emailOtp and smsOtp

---
+++ Backend/src/config/otp.js
@@ -0,0 +1,17 @@
+export const emailOtpConfig = {
+    otpLength:6, // Length of OTP
+    digits: true, // Include digits in the OTP or not
+    lowerCaseAlphabets: false, // Include lowercase alphabets or not
+    upperCaseAlphabets: false, // Include uppercase alphabets or not
+    specialChars: false, // Include special characters or not
+    expiryTime: 5 * 60 * 1000 // Expiry time in milliseconds
+}
+
+export const smsOtpConfig = {
+    otpLength:6, // Length of OTP
+    digits: true, // Include digits in the OTP or not
+    lowerCaseAlphabets: false, // Include lowercase alphabets or not
+    upperCaseAlphabets: false, // Include uppercase alphabets or not
+    specialChars: false, // Include special characters or not
+    expiryTime: 5 * 60 * 1000 //Expiry time in milliseconds
+}
