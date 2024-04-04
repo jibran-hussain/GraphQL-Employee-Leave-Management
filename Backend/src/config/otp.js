dc116a3971eaac4859f2168e7ebd9acfbdecc92d
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-04-04 15:48:13 +0530

    modified configuraiton objects related to email

--- Backend/src/config/otp.js
+++ Backend/src/config/otp.js
@@ -5,6 +5,9 @@ export const emailOtpConfig = {
     upperCaseAlphabets: false, // Include uppercase alphabets or not
     specialChars: false, // Include special characters or not
     expiryTime: 5 * 60 * 1000, // Expiry time in milliseconds
+    resendLimit: 3, // Resend OTP limit
+    maxResendDuration: 10 * 60 * 1000,
+    cooldownPeriod: 1 * 60 * 1000 //Cooldown Period in milliseconds
 }
 
 export const smsOtpConfig = {
@@ -17,8 +20,3 @@ export const smsOtpConfig = {
     resendLimit: 10 * 60 * 1000 // Resend limit in milliseconds
 }
 
-export const resendEmailOtpConfig = {
-    resendLimit: 3, // Resend OTP limit
-    maxResendDuration: 1 * 60 * 1000,
-    cooldownPeriod: 10 * 60 * 1000 //Cooldonw Period in milliseconds
-}
