79561d4c81fd197e0589202c312df3a76ad34b29
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-04-04 12:41:44 +0530

    added config for resending eamil OTP and modified other config objects also

--- Backend/src/config/otp.js
+++ Backend/src/config/otp.js
@@ -4,7 +4,7 @@ export const emailOtpConfig = {
     lowerCaseAlphabets: false, // Include lowercase alphabets or not
     upperCaseAlphabets: false, // Include uppercase alphabets or not
     specialChars: false, // Include special characters or not
-    expiryTime: 5 * 60 * 1000 // Expiry time in milliseconds
+    expiryTime: 5 * 60 * 1000, // Expiry time in milliseconds
 }
 
 export const smsOtpConfig = {
@@ -13,5 +13,12 @@ export const smsOtpConfig = {
     lowerCaseAlphabets: false, // Include lowercase alphabets or not
     upperCaseAlphabets: false, // Include uppercase alphabets or not
     specialChars: false, // Include special characters or not
-    expiryTime: 5 * 60 * 1000 //Expiry time in milliseconds
+    expiryTime: 5 * 60 * 1000, //Expiry time in milliseconds
+    resendLimit: 10 * 60 * 1000 // Resend limit in milliseconds
+}
+
+export const resendEmailOtpConfig = {
+    resendLimit: 3, // Resend OTP limit
+    maxResendDuration: 1 * 60 * 1000,
+    cooldownPeriod: 10 * 60 * 1000 //Cooldonw Period in milliseconds
 }
