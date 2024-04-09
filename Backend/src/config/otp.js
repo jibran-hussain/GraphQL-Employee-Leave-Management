546b2e85f18fc1d16dd53b933b75f6147282f8d9
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-04-09 11:37:40 +0530

    minor changes

--- Backend/src/config/otp.js
+++ Backend/src/config/otp.js
@@ -1,11 +1,11 @@
 export const emailOtpConfig = {
-    otpLength:6, // Length of OTP
+    otpLength:4, // Length of OTP
     digits: true, // Include digits in the OTP or not
     lowerCaseAlphabets: false, // Include lowercase alphabets or not
     upperCaseAlphabets: false, // Include uppercase alphabets or not
     specialChars: false, // Include special characters or not
     expiryTime: 5 * 60 * 1000, // Expiry time in milliseconds
-    resendLimit: 3, // Resend OTP limit
+    resendLimit: 2, // Resend OTP limit
     maxResendDuration: 10 * 60 * 1000,
     cooldownPeriod: 1 * 60 * 1000 //Cooldown Period in milliseconds
 }
